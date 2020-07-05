20b84fd240b94ae9a893cffde9d731b2ae009c06
nicolas <nicolas065@gmail.com>
2020-07-05 13:43:13 +1000

    create the necessary tests for refactor

--- TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,5 +1,6 @@
 package tddmicroexercises.telemetrysystem;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -8,7 +9,7 @@ import static org.mockito.Mockito.*;
 
 public class TelemetryDiagnosticControlsTest {
     @Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+    public void checkTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
         // GIVEN
         var telemetryClient = Mockito.mock(TelemetryClient.class);
         var telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
@@ -21,5 +22,39 @@ public class TelemetryDiagnosticControlsTest {
         // THEN
         assertEquals("it works", telemetryDiagnosticControls.getDiagnosticInfo());
         verify(telemetryClient, times(1)).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
+    }
+
+    @Test
+    public void checkTransmission_should_disconnect_and_if_online_status_is_false_reconnect() throws Exception {
+        // GIVEN
+        var telemetryClient = Mockito.mock(TelemetryClient.class);
+        var telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
+
+        doReturn(false).doReturn(true).when(telemetryClient).getOnlineStatus();
+        when(telemetryClient.receive()).thenReturn("it works");
+
+        // WHEN
+        telemetryDiagnosticControls.checkTransmission();
+
+        // THEN
+        assertEquals("it works", telemetryDiagnosticControls.getDiagnosticInfo());
+        verify(telemetryClient, times(1)).disconnect();
+        verify(telemetryClient, times(1)).connect("*111#");
+    }
+
+    @Test
+    public void checkTransmission_should_throw_an_exception_if_cannot_reconnect_but_try_3_times() throws Exception {
+        // GIVEN
+        var telemetryClient = Mockito.mock(TelemetryClient.class);
+        var telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
+
+        when(telemetryClient.getOnlineStatus()).thenReturn(false);
+
+        // WHEN
+        var thrown = Assertions.assertThrows(Exception.class, () -> telemetryDiagnosticControls.checkTransmission());
+
+        // THEN
+        Assertions.assertEquals("Unable to connect.", thrown.getMessage());
+        verify(telemetryClient, times(3)).connect("*111#");
     }
 }
